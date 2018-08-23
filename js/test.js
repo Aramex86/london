905d394a6977873d8bdf0e5a84a5d3e1cf5c163d
Aramex <slavbez@mail.ru>
2018-08-23 16:13:18 +0300

    testimonials

--- js/test.js
+++ js/test.js
@@ -1,1 +1,24 @@
+$(document).ready(function () {
+
+    $(document).ready(function(){
+        $('.thirdslider').bxSlider({
+            maxSlides: 4,
+            moveSlides: 1,
+            slideWidth: 370,
+            pager:false,
+        });
+
+    });
+
+
+    //alert("second block img hover");
+});
+
+
+
+
+
+
+
+
 
