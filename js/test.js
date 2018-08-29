554d0ce8321417c044e93555ddf1fda6621671a6
Aramex <slavbez@mail.ru>
2018-08-29 18:24:26 +0300

    change6

--- js/test.js
+++ js/test.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
-
-    $(document).ready(function(){
-        $('.thirdslider').bxSlider({
+    $('.thirdslider').bxSlider({
             maxSlides: 4,
             moveSlides: 1,
             slideWidth: 370,
             pager:false,
             randomStart:true,
+            auto:true,
 
         });
-
+        $('.slider').bxSlider({
+        moveSlides: 1,
+        maxSlides:5,
+        slideWidth: 280,
+        pager: false,
+        randomStart: true,
+    });
+    $('.headerslider').bxSlider({
+        pager:false,
+        auto:true,
+        autoHover:true,
+        maxSlides:1,
+        moveSlides:1,
+        minSlides:1,
     });
 
-
-    //alert("second block img hover");
 });
 
 
