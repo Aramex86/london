c5583b17e1bc7efdadc73cd1fe2d2699b4149ead
Aramex <slavbez@mail.ru>
2018-09-01 15:57:53 +0300

    425px start

--- js/test.js
+++ js/test.js
@@ -1,29 +1,48 @@
 $(document).ready(function () {
     $('.thirdslider').bxSlider({
-            maxSlides: 4,
-            moveSlides: 1,
-            slideWidth: 370,
-            pager:false,
-            randomStart:true,
-            auto:true,
-
-        });
-        $('.slider').bxSlider({
+        maxSlides: 4,
         moveSlides: 1,
-        maxSlides:5,
+        slideWidth: 370,
+        pager: false,
+        randomStart: true,
+        auto: true,
+
+    });
+    $('.slider').bxSlider({
+        moveSlides: 1,
+        maxSlides: 5,
         slideWidth: 365,
         pager: false,
         randomStart: true,
-        auto:true,
+        auto: true,
     });
     $('.headerslider').bxSlider({
-        pager:false,
-        auto:true,
-        autoHover:true,
-        maxSlides:1,
-        moveSlides:1,
-        minSlides:1,
+        pager: false,
+        auto: true,
+        autoHover: true,
+        maxSlides: 1,
+        moveSlides: 1,
+        minSlides: 1,
     });
+    $(".menubtnshow").click(function () {
+       $(".mobile-menuwrap").show(1000);
+       $(".menubtnshow").css("display","none");
+       $(".menubtnhide").css("display","block");
+    });
+    $(".menubtnhide").click(function () {
+        $(".mobile-menuwrap").hide(1000);
+        $(".menubtnshow").css("display","block");
+        $(".menubtnhide").css("display","none");
+    });
+
+
+
+
+
+
+
+
+
 
 });
 
