398e77a0392ec768cebb09fbc3e9977a00a7b51e
Aramex <slavbez@mail.ru>
2018-09-14 00:42:44 +0300

    done

--- js/test.js
+++ js/test.js
@@ -109,6 +109,89 @@ $(document).ready(function () {
             $(".hoverroom:eq(11)").fadeOut(500);
         });
     });
+    /***************************
+     * aptop
+     */
+    $('.aptop').find($('.like')).css({
+        left:580,
+        top:408,
+
+    });
+    $(".aptop:eq(0) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(0)").fadeIn(500);
+        $(".hoverroomtop:eq(0)").mouseleave(function () {
+            $(".hoverroomtop:eq(0)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(1) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(1)").fadeIn(500);
+        $(".hoverroomtop:eq(1)").mouseleave(function () {
+            $(".hoverroomtop:eq(1)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(2) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(2)").fadeIn(500);
+        $(".hoverroomtop:eq(2)").mouseleave(function () {
+            $(".hoverroomtop:eq(2)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(3) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(3)").fadeIn(500);
+        $(".hoverroomtop:eq(3)").mouseleave(function () {
+            $(".hoverroomtop:eq(3)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(4) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(4)").fadeIn(500);
+        $(".hoverroomtop:eq(4)").mouseleave(function () {
+            $(".hoverroomtop:eq(4)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(5) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(5)").fadeIn(500);
+        $(".hoverroomtop:eq(5)").mouseleave(function () {
+            $(".hoverroomtop:eq(5)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(6) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(6)").fadeIn(500);
+        $(".hoverroomtop:eq(6)").mouseleave(function () {
+            $(".hoverroomtop:eq(6)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(7) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(7)").fadeIn(500);
+        $(".hoverroomtop:eq(7)").mouseleave(function () {
+            $(".hoverroomtop:eq(7)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(8) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(8)").fadeIn(500);
+        $(".hoverroomtop:eq(8)").mouseleave(function () {
+            $(".hoverroomtop:eq(8)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(9) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(9)").fadeIn(500);
+        $(".hoverroomtop:eq(9)").mouseleave(function () {
+            $(".hoverroomtop:eq(9)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(10) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(10)").fadeIn(500);
+        $(".hoverroomtop:eq(10)").mouseleave(function () {
+            $(".hoverroomtop:eq(10)").fadeOut(500);
+        });
+    });
+    $(".aptop:eq(11) img:nth-child(1)").mouseover(function () {
+        $(".hoverroomtop:eq(11)").fadeIn(500);
+        $(".hoverroomtop:eq(11)").mouseleave(function () {
+            $(".hoverroomtop:eq(11)").fadeOut(500);
+        });
+    });
+
+
+
     $(".like").click(function () {
        $(this).css('background-color','#0089f6');
     });
@@ -173,10 +256,10 @@ $(document).ready(function () {
 
    $('.day').click(function () {
      $.each([6,4,8,2,10,11],function (i ,hide) {
-         $('.apartaments').eq(hide).fadeIn(1000);
+        $('.apartaments').eq(hide).fadeIn(1000);
      });
        $.each([0,1,9,3,7,5],function (i , show) {
-           $('.apartaments').eq(show).fadeOut(1000);
+         $('.apartaments').eq(show).fadeOut(1000);
        });
    });
    $('.month').click(function () {
@@ -244,6 +327,137 @@ $(document).ready(function () {
                     $('html, body').animate({scrollTop:0},'slow');
                     return false
                 });
+
+    /*********************************
+     * SEARCh engine
+     */
+    var france = $('.wrap-top').find($('.aptop:contains("France")'));
+    var germany = $('.wrap-top').find($('.aptop:contains("Germany")'));
+    var london = $('.wrap-top').find($('.aptop:contains("London")'));
+    var denmark = $('.wrap-top').find($('.aptop:contains("Denmark")'));
+    var rome = $('.wrap-top').find($('.aptop:contains("Rome")'));
+    $('.wrap-top').find($('.aptop')).hide();
+    $('#search').click(function () {
+       if ($('.location').val() == 1 && $('.guest').val()==2 && $('.room').val()==2) {
+           $(london).eq(2).fadeIn(500);
+           $(london).filter(':eq(0),:eq(1),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(france).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('#clear').css('display','inline-block');
+       }else if($('.location').val() == 1 && $('.guest').val()==3 && $('.room').val()==3) {
+           $(london).eq(3).fadeIn(500);
+           $(london).filter(':eq(0),:eq(1),:eq(2)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(france).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('#clear').css('display','inline-block');
+       }else if($('.location').val() == 1 && $('.guest').val()==4 && $('.room').val()==4) {
+            $(london).eq(1).fadeIn(500);
+           $(london).filter(':eq(0),:eq(2),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(france).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+        }else if ($('.location').val() == 1 && $('.guest').val()==3 && $('.room').val()==4) {
+           $(london).eq(0).fadeIn(500);
+           $(london).filter(':eq(2),:eq(1),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(france).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+       }else if ($('.location').val() == 2 && $('.guest').val()==4 && $('.room').val()==3) {
+            $(germany).eq(0).fadeIn(500);
+            $(germany).filter(':eq(1)').hide();
+           $(london).filter(':eq(0),:eq(1),:eq(2),:eq(3)').hide();
+           $(france).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+        }else if($('.location').val() == 2 && $('.guest').val()==3 && $('.room').val()==2) {
+           $(germany).eq(1).fadeIn(500);
+           $(germany).filter(':eq(0)').hide();
+           $(london).filter(':eq(0),:eq(1),:eq(2),:eq(3)').hide();
+           $(france).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+       }else if ($('.location').val() == 3 && $('.guest').val()==3 && $('.room').val()==3) {
+            $(france).eq(0).fadeIn(500);
+            $(france).filter(':eq(1)').hide();
+           $(london).filter(':eq(0),:eq(1),:eq(2),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+        }else if($('.location').val() == 3 && $('.guest').val()==4 && $('.room').val()==4) {
+            $(france).eq(1).fadeIn(500);
+            $(france).filter(':eq(0)').hide();
+           $(london).filter(':eq(0),:eq(1),:eq(2),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(rome).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+        }else if ($('.location').val() == 4 && $('.guest').val()==2 && $('.room').val()==2) {
+            $(rome).eq(0).fadeIn(500);
+           $(london).filter(':eq(0),:eq(1),:eq(2),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(france).filter(':eq(0)').hide();
+           $(denmark).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+        }else if ($('.location').val() == 5 && $('.guest').val()==4 && $('.room').val()==4) {
+            $(denmark).eq(0).fadeIn(500);
+            $(denmark).filter(':eq(1)').hide();
+           $(london).filter(':eq(0),:eq(1),:eq(2),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(france).filter(':eq(0)').hide();
+           $(rome).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+        }else if($('.location').val() == 5 && $('.guest').val()==3 && $('.room').val()==3) {
+            $(denmark).eq(1).fadeIn(500);
+            $(denmark).filter(':eq(0)').hide();
+           $(london).filter(':eq(0),:eq(1),:eq(2),:eq(3)').hide();
+           $(germany).filter(':eq(0),:eq(1)').hide();
+           $(france).filter(':eq(0)').hide();
+           $(rome).filter(':eq(0),:eq(1)').hide();
+           $('.location').val(0);
+           $('#clear').css('display','inline-block');
+        }else if($('.location').val()==0){
+           $(france).css('display','none');
+           $(germany).css('display','none');
+           $(london).css('display','none');
+           $(denmark).css('display','none');
+           $(rome).css('display','none');
+           $('.guest').val(1);
+           $('.room').val(1);
+       }
+        else {
+            alert('NO MATCHES');
+       }
+    });
+        $('#clear').click(function () {
+            $('.location').val(0);
+            $('.guest').val(1);
+            $('.room').val(1);
+            if ($('.location').val() == 0 && $('.guest').val()==1 && $('.room').val()==1){
+                $('.aptop').fadeOut(500);
+                $('#clear').fadeOut(500);
+            }
+        });
+
+
+
 
 
 
